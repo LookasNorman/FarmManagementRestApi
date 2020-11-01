4848ccf9c819f6a6b881dfc061bb9e1205546123
LookasNorman <lookasziebice@gmail.com>
2020-11-01 11:21:50 +0100

    Added certificate entity

---
+++ src/Entity/Certificate.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CertificateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CertificateRepository::class)
+ */
+class Certificate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=CertificateType::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $certificateType;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Firms::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $firm;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $certificateNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCertificateType(): ?CertificateType
+    {
+        return $this->certificateType;
+    }
+
+    public function setCertificateType(?CertificateType $certificateType): self
+    {
+        $this->certificateType = $certificateType;
+
+        return $this;
+    }
+
+    public function getFirm(): ?Firms
+    {
+        return $this->firm;
+    }
+
+    public function setFirm(?Firms $firm): self
+    {
+        $this->firm = $firm;
+
+        return $this;
+    }
+
+    public function getCertificateNumber(): ?string
+    {
+        return $this->certificateNumber;
+    }
+
+    public function setCertificateNumber(string $certificateNumber): self
+    {
+        $this->certificateNumber = $certificateNumber;
+
+        return $this;
+    }
+}
