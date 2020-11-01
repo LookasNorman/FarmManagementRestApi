abc0109e58b6f7dca52367f577638e0d647313e7
LookasNorman <lookasziebice@gmail.com>
2020-11-01 10:25:35 +0100

    Added counter entity

---
+++ src/Entity/Counter.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CounterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CounterRepository::class)
+ */
+class Counter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=CounterType::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $counterType;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $IU;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $active;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCounterType(): ?CounterType
+    {
+        return $this->counterType;
+    }
+
+    public function setCounterType(?CounterType $counterType): self
+    {
+        $this->counterType = $counterType;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIU(): ?string
+    {
+        return $this->IU;
+    }
+
+    public function setIU(string $IU): self
+    {
+        $this->IU = $IU;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+}
