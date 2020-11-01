4ac7a16bfcb09b66ce1d7c17908c4c0ed0e5c08d
LookasNorman <lookasziebice@gmail.com>
2020-11-01 10:27:18 +0100

    Added counter state entity

---
+++ src/Entity/CounterState.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CounterStateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CounterStateRepository::class)
+ */
+class CounterState
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Counter::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $counter;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=3)
+     */
+    private $state;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCounter(): ?Counter
+    {
+        return $this->counter;
+    }
+
+    public function setCounter(?Counter $counter): self
+    {
+        $this->counter = $counter;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
