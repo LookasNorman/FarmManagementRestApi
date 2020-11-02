e87976af3f9c7a761e40eee79a66cf7073fb5306
LookasNorman <lookasziebice@gmail.com>
2020-11-02 20:55:35 +0100

    Delete operation for item: DELETE, PATCH

--- src/Entity/Firms.php
+++ src/Entity/Firms.php
@@ -7,7 +7,10 @@ use App\Repository\FirmsRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get", "put"}
+ * )
  * @ORM\Entity(repositoryClass=FirmsRepository::class)
  */
 class Firms
