282d796828b462a145cc49eb80462a737af74aac
LookasNorman <lookasziebice@gmail.com>
2020-11-21 20:38:05 +0100

    Added some data for daily production report

--- src/Controller/ProductionDailyReportController.php
+++ src/Controller/ProductionDailyReportController.php
@@ -14,13 +14,19 @@ class ProductionDailyReportController extends AbstractController
         $date = new \DateTime($date);
         $herdsRepository = $this->getDoctrine()->getRepository(Herds::class);
         $herdDataRepository = $this->getDoctrine()->getRepository(HerdsData::class);
+        $data = [];
 
-        $herdsInProduction = $herdsRepository->herdHatchingDateInProductionOnDate($date);
-        foreach ($herdsInProduction as $hatchingDate){
-
+        $hatchingDateHerdsInProduction = $herdsRepository->herdHatchingDateInProductionOnDate($date);
+        foreach ($hatchingDateHerdsInProduction as $key => $hatchingDate) {
+            $herds = $herdsRepository->findBy(['hatchingDate' => $hatchingDate['hatchingDate']]);
+            $herdsData = [];
+            foreach ($herds as $key2 => $herd) {
+                $herdsData [] = $herdDataRepository->herdsState($herd, $date);
+            }
+            $data[$key] = $herdsData;
         }
 
-        $response = new Response(json_encode($herdsInProduction));
+        $response = new Response(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
